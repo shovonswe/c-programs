61e650df67dd66d58b37914a1c209fac1ccdc833
shovonswe <hasanshovon864@gmail.com>
2025-01-10 22:21:45 +0600

    two problem solved

---
+++ DD.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+  
+char a[]="shovon islam";
+
+  
+  
+   int i=0, len=0;
+
+   while(a[i]!='\0'){
+    i++;
+    len++;
+   }
+   printf("%d\n",len);
+
+   
+     return 0;
+
+
+
+}
---
+++ h.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+  int a[2][3]={{1,2,3},{4,5,6}},i,j;
+  for(i=0; i<2; i++){
+    for(j=0; j<3; j++)
+    {
+      printf("%d",a[i][j]);
+      
+  }
+   return 0;
+}
